87c9d67f4217b23a81a13f7e191d7f372a22c29c
Lindsay <lindsay.murphy2@gmail.com>
2023-04-28 11:18:34 -0400

    move files into dbt folder

--- macros/convert_empty_string_to_null.sql
+++
@@ -1,6 +0,0 @@
-
-{% macro convert_empty_string_to_null(empty_column_name) %}
-
-    NULLIF({{ empty_column_name }}, '')
-
-{% endmacro %}
--- macros/convert_timestamp_to_now.sql
+++
@@ -1,9 +0,0 @@
--- Returns the current timestamp in the UTC time zone
-{% macro now_utc() %}
-    CURRENT_TIMESTAMP AT TIME ZONE 'UTC'
-{% endmacro %}
-
--- Returns the current timestamp in the America/Toronto time zone
-{% macro now_eastern() %}
-    CURRENT_TIMESTAMP AT TIME ZONE 'America/Toronto'
-{% endmacro %}
