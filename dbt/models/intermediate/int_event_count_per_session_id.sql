7a2324e6dae34a54dad1582a136a00f6d0a64129
Lindsay <lindsay.murphy2@gmail.com>
2023-06-04 21:09:16 -0400

    update sqlfluff config file

--- dbt/models/intermediate/int_event_count_per_session_id.sql
+++ dbt/models/intermediate/int_event_count_per_session_id.sql
@@ -3,7 +3,7 @@
 SELECT
     session_id,
     {%- for event_name in event_names -%}
-    SUM(CASE WHEN event_name = '{{event_name}}' THEN 1 ELSE 0 END) AS {{event_name|replace(" ", "_")|lower}}_present,
+    SUM(CASE WHEN event_name = '{{ event_name }}' THEN 1 ELSE 0 END) AS {{ event_name|replace(" ", "_")|lower }}_present,
     {% endfor %}
     SUM(event_id) AS number
 FROM {{ ref('stg_bingeflix_events') }}
--- dbt/models/staging/bingeflix/stg_bingeflix_users.sql
+++ dbt/models/staging/bingeflix/stg_bingeflix_users.sql
@@ -3,7 +3,6 @@ with
 source as (
     select
         user_id,
-        {{ rolling_average_7_periods('user_id', 'NULL', 'user_id') }},
         created_at,
         phone_number,
         deleted_at,
