424fe65b0086d8a08da78c1656749272b8aeb162
Jeffrey Asselin <jasselin68@gmail.com>
2023-08-08 14:49:01 +0000

    created rolling_avg_n_periods macro; created .yml file and documented it; updated pre-commit-config file

---
+++ macros/rolling_average_n_periods.sql
@@ -0,0 +1,7 @@
+{% macro rolling_average_n_periods(column_name, partition_by, order_by, number_of_periods=7) %}
+    avg( {{ column_name }} ) OVER (
+                PARTITION BY {{ partition_by }}
+                ORDER BY {{ order_by }}
+                ROWS BETWEEN ({{ number_of_periods }} - 1) PRECEDING AND CURRENT ROW
+            ) AS avg_{{ number_of_periods }}_periods_{{ column_name }}
+{% endmacro %}
