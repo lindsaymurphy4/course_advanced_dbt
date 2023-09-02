66dbbb1ebbb1dbc7ca1e8ceb51d551a94b70ff7c
Kaushikm671 M <kaushikm.ju@gmail.com>
2023-09-02 01:47:07 +0000

    message

--- models/marts/core/dim_users.sql
+++ models/marts/core/dim_users.sql
@@ -31,9 +31,7 @@ final AS (
         TRUNCATE(DATEDIFF(MONTH, birthdate, CURRENT_DATE)/12) AS current_age,
         TRUNCATE(DATEDIFF(MONTH, birthdate, created_at)/12) AS age_at_acquisition,
         region,
-        country,
-        usf.first_subscription_starts_at,
-        usf.count_of_subscriptions
+        country
     FROM
         users AS u
         LEFT JOIN users_subscription_facts AS usf ON u.user_id = usf.user_id
