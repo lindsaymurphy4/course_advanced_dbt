be5277ad265c29a35cbb27334126c3235d9ef479
AlanCortesK <alan.cortes@kueski.com>
2024-01-27 18:50:45 +0000

    [week_1_project] Lint sql code using sqlfluff fix

--- models/intermediate/product/int_sessions_pivoted_to_event_names.sql
+++ models/intermediate/product/int_sessions_pivoted_to_event_names.sql
@@ -1,11 +1,18 @@
-{% set event_names = dbt_utils.get_column_values(table=ref('stg_bingeflix__events'), column='event_name') -%}
+{%
+    set event_names = dbt_utils.get_column_values(
+        table=ref('stg_bingeflix__events'), column='event_name'
+        ) 
+-%}
 
 SELECT
     session_id,
     user_id,
+
     {% for event_name in event_names %}
-    SUM(CASE WHEN event_name = '{{ event_name }}' THEN 1 ELSE 0 END) AS {{ event_name|replace(" ", "_")|lower }}_count
+    SUM(CASE WHEN event_name = '{{ event_name }}' THEN 1 ELSE 0 END)
+        AS {{ event_name|replace(" ", "_")|lower }}_count
     {% if not loop.last %},{% endif -%}
+
     {% endfor %}
 FROM
     {{ ref('stg_bingeflix__events') }}
